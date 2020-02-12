40e1a138fb1b9945d79088ffbc7db1cfd15315b0
Anders Tøgersen <au563513@uni.au.dk>
2020-02-12 14:54:20 +0100

    tour de test

---
+++ ECS.Unit.Test/EcsUnitTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace ECS.Unit.Test
+{
+    [TestFixture]
+    public class EcsUnitTest
+    {
+        [TestCase()]
+        public void Method1()
+        {
+
+        }
+    }
+}
